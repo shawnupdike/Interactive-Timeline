817abe1146c7bc14cf97d671589a14859e351a01
rogueupdike <42945566+rogueupdike@users.noreply.github.com>
2019-10-03 12:55:22 -0400

    Added code form code pen and finalized HTML Content

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 $(document).foundation()
+
+// when the page loads, hide all panels except for the one with the id 'one'
+$('.panel').not('#one').hide();
+
+// on click, hide the currently active panel and show the one selected
+$('nav').on('click','a',function(e){
+  // don't jump to the panel (the default action)
+  e.preventDefault();
+  // get the id of the selected panel
+  var curr = $(this).attr('href');
+  // hide all the panels
+  $('.panel').hide();
+  // show the selected panel
+  $(curr).show();
+});
